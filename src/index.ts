f409ce01dd88e6db1f8b0550633d90e1e7ef2534
Shashank Rai <shashankrai0805@gmail.com>
2025-07-09 22:40:38 +0530

    feat: Add Hono API routes with Prisma Accelerate setup

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,39 @@
 import { Hono } from 'hono'
+import { PrismaClient } from './generated/prisma'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-const app = new Hono()
 
-app.get('/', (c) => {
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string
+  }
+}>()
+
+app.post('/api/v1/user/signup', (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+
+  return c.text("Hello Hono!");
+})
+
+app.post('/api/v1/user/signin', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.post('/api/v1/blog', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.put('/api/v1/blog', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.get('/api/v1/blog/:id', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.get('/api/v1/blog/bulk', (c) => {
   return c.text('Hello Hono!')
 })
 
