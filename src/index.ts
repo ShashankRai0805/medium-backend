62f5514d17ef49c25adc0a8c6fdfd37cb68970a9
Shashank Rai <shashankrai0805@gmail.com>
2025-07-09 23:23:55 +0530

    feat: Add JWT authentication to user signup

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,41 @@
 import { Hono } from 'hono'
-import { PrismaClient } from './generated/prisma'
+import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
-
+import {decode, sign, verify} from 'hono/jwt'
 
 const app = new Hono<{
   Bindings: {
-    DATABASE_URL: string
+    DATABASE_URL: string,
+    JWT_SECRET: string
   }
 }>()
 
-app.post('/api/v1/user/signup', (c) => {
+app.post('/api/v1/user/signup', async (c) => {
+  const body = await c.req.json();
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate())
 
-  return c.text("Hello Hono!");
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: body.password,
+        name: body.name
+
+      }
+    })
+    const jwt = await sign({
+      id: user.id
+    }, c.env.JWT_SECRET)
+
+    return c.text(jwt);
+    
+  } catch (error) {
+    console.error('Error creating user:', error);
+    c.status(411);
+    return c.text("Invalid request");
+  }
 })
 
 app.post('/api/v1/user/signin', (c) => {
